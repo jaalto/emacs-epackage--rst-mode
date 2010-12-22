3013f3b2a3c415bb737db282e658a8c43ea60170
Jari Aalto <jari.aalto@cante.net>
2010-12-22 14:00:48 +0200

    rst-mode-uninstall.el: new

---
+++ epackage/rst-mode-uninstall.el
@@ -0,0 +1,7 @@
+
+(if (string< (emacs-version) "23")	;included in core-emacs
+    (setq auto-mode-alist
+	  (delete '("\\.re?st\\'" . rst-mode)
+		  auto-mode-alist)))
+
+(provide 'rst-mode-uninstall)
